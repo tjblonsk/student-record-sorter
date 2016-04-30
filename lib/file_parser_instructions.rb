e2601ab61707f6df29a3bdfb629d1159a27d8a43
Tim <tim.blonski@gmail.com>
2016-04-30 17:14:50 -0400

    Add file parse instructions mapper module.

---
+++ lib/file_parser_instructions.rb
@@ -0,0 +1,10 @@
+module FileParserInstructions
+  MAPPER = {
+    comma: {
+      delimiter: Regexp.new(/,\s/),
+      order: [
+        'last_name', 'first_name', 'campus', 'favorite_color', 'date_of_birth'
+      ]
+    }
+  }
+end
--- spec/input/file_parser_spec.rb
+++ spec/input/file_parser_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 require './app/input/file_parser'
+require './lib/file_parser_instructions'
+include FileParserInstructions
 
 RSpec.describe FileParser do
   let(:path)            { './spec/fixtures/input.txt' }
   let(:file_parser)     { FileParser.new(path) }
-  let(:comma_regexp)    { Regexp.new(/,\s/) }
+  let(:comma_regexp)    { MAPPER[:comma][:delimiter] }
   let(:comma_array)     { File.open(path).readlines[0].split(comma_regexp) }
   let(:invalid_parser)  { FileParser.new(nil) }
 
