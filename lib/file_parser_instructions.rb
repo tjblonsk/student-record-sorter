93dea07fa0bb2a5bd64ec9c83e0dbde9bd73aba1
Tim <tim.blonski@gmail.com>
2016-05-01 13:49:34 -0400

    Add dollar and pipe to mappings.

--- lib/file_parser_instructions.rb
+++ lib/file_parser_instructions.rb
@@ -5,6 +5,19 @@ module FileParserInstructions
       order: [
         :last_name, :first_name, :campus, :favorite_color, :date_of_birth
       ]
+    },
+    dollar: {
+      delimiter: Regexp.new(/\s\$\s/),
+      order: [
+        :last_name, :first_name, :middle_initial, :campus, :date_of_birth, :favorite_color
+      ]
+    },
+
+    pipe: {
+      delimiter: Regexp.new(/\s\|\s/),
+      order: [
+        :last_name, :first_name, :middle_initial, :campus, :favorite_color, :date_of_birth
+      ]
     }
   }
 end
--- spec/input/file_parser_spec.rb
+++ spec/input/file_parser_spec.rb
@@ -5,15 +5,33 @@ include FileParserInstructions
 
 describe FileParser do
   let(:path)            { './spec/fixtures/input.txt' }
+  let(:input_fixture)   { File.open(path).readlines }
+
   let(:file_parser)     { FileParser.new(path) }
-  let(:comma_regexp)    { MAPPER[:comma][:delimiter] }
-  let(:comma_array)     { File.open(path).readlines[0].split(comma_regexp) }
   let(:invalid_parser)  { FileParser.new(nil) }
 
-  context 'comma delimiter' do
-    it 'must split into an array' do
-      expect(file_parser.split_by(comma_regexp)[0]).to eq comma_array
-    end
+  let(:comma_line_idx)      { 0 }
+  let(:comma_regexp)    { MAPPER[:comma][:delimiter] }
+  let(:comma_array)     { input_fixture[comma_line_idx].split(comma_regexp) }
+
+  let(:dollar_line_idx)  { 1 }
+  let(:dollar_regexp)    { MAPPER[:dollar][:delimiter] }
+  let(:dollar_array)     { input_fixture[dollar_line_idx].split(dollar_regexp) }
+
+  let(:pipe_line_idx)  { 2 }
+  let(:pipe_regexp)    { MAPPER[:pipe][:delimiter] }
+  let(:pipe_array)     { input_fixture[pipe_line_idx].split(pipe_regexp) }
+
+  it 'must split into an array by comma' do
+    expect(file_parser.split_by(comma_regexp)[comma_line_idx]).to eq comma_array
+  end
+
+  it 'must split into an array by dollar' do
+    expect(file_parser.split_by(dollar_regexp)[dollar_line_idx]).to eq dollar_array
+  end
+
+  it 'must split into an array by pipe' do
+    expect(file_parser.split_by(pipe_regexp)[pipe_line_idx]).to eq pipe_array
   end
 
   it 'must raise an error if no file path is provided' do
