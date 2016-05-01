e2a8c00afab4ad00f2795924702d105bf7d3a6af
Tim <tim.blonski@gmail.com>
2016-05-01 12:10:12 -0400

    Refactor student to build attrs with class method.

--- app/collections/student.rb
+++ app/collections/student.rb
@@ -1,13 +1,6 @@
 require 'ostruct'
 
 class Student < OpenStruct
-  def self.build_student(row, order)
-    attrs = build_attrs(row, order)
-    new(attrs)
-  end
-
-  private
-
   def self.build_attrs(row, order)
     order.zip(row).to_h
   end
--- spec/collections/student_spec.rb
+++ spec/collections/student_spec.rb
@@ -7,8 +7,13 @@ describe Student do
   let(:comma_regexp)    { MAPPER[:comma][:delimiter] }
   let(:comma_order)     { MAPPER[:comma][:order] }
   let(:comma_row)       { File.open(path).readlines[0].split(comma_regexp) }
-  let(:student)         { Student.build_student(comma_row, comma_order) }
+  let(:student_attrs)   { Student.build_attrs(comma_row, comma_order) }
+  let(:student)         { Student.new(student_attrs) }
   let(:comma_attrs)     { comma_order.zip(comma_row).to_h }
+
+  it 'builds attrs from a row' do
+    expect(student_attrs).to eq comma_attrs
+  end
 
   context 'comma order' do
     it 'builds a struct given a row and order of attrs' do
